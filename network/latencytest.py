ffbaf1f7018d519a9704fc2a6f229e297d3d39f8
chemhack <blazeblue@gmail.com>
2012-12-02 20:54:04 +0100

    fix download bug

--- network/latencytest.py
+++ network/latencytest.py
@@ -21,12 +21,14 @@ DOWNLOAD_FILES = [
     'random3000x3000.jpg',
     'random4000x4000.jpg'
 ]
-MAX_DOWNLOAD_TIME = 60 * 1 #Limit: 1min
-MIN_DOWNLOAD_TIME = 30 #Limit: 30s
-MAX_UPLOAD_TIME = 60 * 1 #Limit: 1min
+MAX_DOWNLOAD_TIME = 30 #Limit: 30s
+MIN_DOWNLOAD_TIME = 15 #Limit: 15s
+MAX_UPLOAD_TIME = 30 #Limit: 30s
 MIN_UPLOAD_TIME = 15 #Limit: 15s
+
 class SpeedTest:
-    pingQueue=Queue()
+    pingQueue = Queue()
+
     def download(self, baseUrl):
         parsedUrl = urlparse(baseUrl)
         connection = httplib.HTTPConnection(parsedUrl.netloc)
@@ -35,7 +37,7 @@ class SpeedTest:
         time_count = 0
         start_time = time()
         shouldStop = False
-        while True:
+        while not(shouldStop or time_count > MIN_DOWNLOAD_TIME):
             for fileName in DOWNLOAD_FILES:
                 try:
                     connection.request('GET', parsedUrl.path + fileName + '?x=' + str(int(time() * 1000)), None,
@@ -49,15 +51,16 @@ class SpeedTest:
                     while True:
                         # download buffer size = 10KB
                         content = response.read(1024 * 10)
-                        time_count=time() - start_time
+                        time_count = time() - start_time
                         if content:
                             byte_count += len(content)
                             if time_count > MAX_DOWNLOAD_TIME:
+                                response.close()
                                 shouldStop = True
                                 break
                         else:
                             break
-                if shouldStop or time_count>MIN_DOWNLOAD_TIME:
+                if shouldStop:
                     break
         return byte_count * 8 / time_count
 
@@ -66,25 +69,23 @@ class SpeedTest:
         time_count = 0
         byte_count = 0
         shouldStop = False
-        while True:
+        while not(shouldStop or time_count > MIN_DOWNLOAD_TIME):
             connection = httplib.HTTPConnection(parsedUrl.netloc)
             connection.connect()
             connection.request('POST', url, None,
                     {'Connection': 'Keep-Alive', 'Content-Type': 'application/x-www-form-urlencoded',
-                     'Content-Length': 1024*100*1000+1})
+                     'Content-Length': 1024 * 100 * 1000 + 1})
             #send the first byte
             connection.send('0')
-            block='0'*1024*100
+            block = '0' * 1024 * 100
             start = time()
             for x in range(1000):
                 connection.send(block)
                 time_count = time() - start
                 byte_count += len(block)
-                if time_count>MAX_UPLOAD_TIME:
-                    shouldStop=True
+                if time_count > MAX_UPLOAD_TIME:
+                    shouldStop = True
                     break
-            if shouldStop or time_count>MIN_UPLOAD_TIME:
-                break
         return byte_count * 8 / time_count
 
     def ping(self, url):
@@ -111,30 +112,37 @@ class SpeedTest:
         for line in f.readlines():
             data = line.split('\t')
             self.pingQueue.put(data)
-#            print data[3]+", "+data[4] + "\t" + str(self.ping(data[0])['avg'])
-        threads=[]
+        #            print data[3]+", "+data[4] + "\t" + str(self.ping(data[0])['avg'])
+        threads = []
         for x in range(20):
-            thread=Thread(target=self.pingWorker, args=())
+            thread = Thread(target=self.pingWorker, args=())
             thread.start()
             threads.append(thread)
         for thread in threads:
             thread.join()
         return
+
     def pingWorker(self):
         while True:
             if self.pingQueue.empty():
                 break
-            data=self.pingQueue.get()
-            print data[3]+", "+data[4] + "\t" + str(self.ping(data[0])['avg'])
+            data = self.pingQueue.get()
+            print data[3] + ", " + data[4] + "\t" + str(self.ping(data[0])['avg'])
             self.pingQueue.task_done()
         return
 
+
 def main():
     test = SpeedTest()
-    start=time()
-    test.pingAll()
-    print time()-start
-#    print test.upload('http://speedtest.fas.ge/speedtest/upload.aspx')
+    #    start=time()
+    #    test.pingAll()
+    #    print time()-start
+    print test.ping('http://down.yiinet.net/speedtest/speedtest/latency.txt')
+    print test.download('http://down.yiinet.net/speedtest/speedtest/')
+    print test.upload('http://down.yiinet.net/speedtest/speedtest/upload.aspx')
+    print test.ping('http://speed.dtgt.org/speedtest/latency.txt')
+    print test.download('http://speed.dtgt.org/speedtest/')
+    print test.upload('http://speed.dtgt.org/speedtest/upload.php')
     return
 
 if __name__ == '__main__':
